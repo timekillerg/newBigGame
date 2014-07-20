a85aab14a82653d9b857f9b7cce9f20773d32a5e
Eldar <timekillerg@gmail.com>
2014-07-21 01:08:21 +0300

    Eldar: Spider Ball Game

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+
+class BallController : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnMouseUp()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/BallsGenerator.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallsGenerator : MonoBehaviour
+{
+    public GameObject[] balls;
+    public Vector3 ballPosition;
+    public float minXPosition;
+    public float maxXPosition;
+    public int ballsCount;
+    private readonly float PERIOD_BETWEEN_BALLS = 1f;
+    private float _instantiateTime;
+
+    void Start()
+    {
+        _instantiateTime = Time.time;
+    }
+
+    void Update()
+    {
+        if (_instantiateTime + 0.25 < Time.time && GameObject.FindGameObjectsWithTag("Ball").Length < ballsCount)
+        {
+            InstatiateAnyBall();
+            _instantiateTime = Time.time;
+        }
+    }
+
+    private void InstatiateAnyBall()
+    {
+        int ballId = Random.Range(0, balls.Length);
+        float xPosition = Random.Range(minXPosition, maxXPosition);
+        Vector3 newBallPosition = ballPosition;
+        newBallPosition.x = xPosition;
+        Instantiate(balls[ballId], newBallPosition, Quaternion.identity);
+    }
+}
