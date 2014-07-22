02ac43707a60ef863987785c8824e058d18714c8
Eldar <timekillerg@gmail.com>
2014-07-23 01:29:31 +0300

    Eldar: Some fixes

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -1,34 +1,64 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class BallController : MonoBehaviour
 {
-    private Vector3 move;
+    private Vector3 _move;
+    private Vector3 _startPosition;
+    private readonly float WAIT_BEFORE_CHANGE_TO_KINEMATIC = 10f;
+    private readonly float RETURN_BACK_SPEED = 10;
 
     void Start()
     {
-        move = Vector3.zero;
+        _move = Vector3.zero;
+        StartCoroutine(MakeBallKinematicAfterWait(WAIT_BEFORE_CHANGE_TO_KINEMATIC));
     }
 
     void Update()
     {
-        if (move != Vector3.zero)
-            gameObject.transform.position = move;
+        if (_move != Vector3.zero && _move != _startPosition)
+            gameObject.transform.position = _move;
+        else if (_move != Vector3.zero && _move == _startPosition)
+            gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, _move, Time.time * RETURN_BACK_SPEED);
     }
 
     void OnMouseDrag()
     {
-        move = Input.mousePosition;
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, 100))
-        {
-            move = hit.point;
-            move.z = 0.5f;
-        }
+        _move = GetMousePositionAbsolute();
+        _move.z = 0.5f;
+    }
+
+    void OnMouseDown()
+    {
+        _startPosition = GetMousePositionAbsolute();
+        _startPosition.z = 0.5f;
     }
 
     void OnMouseUp()
     {
-        move = Vector3.zero;
+        _move = _startPosition;
+    }
+
+    IEnumerator MakeBallKinematicAfterWait(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        gameObject.rigidbody.isKinematic = true;
+        gameObject.rigidbody.useGravity = false;
+    }
+
+    private Vector3 GetMousePositionAbsolute()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        return (Physics.Raycast(ray, out hit, 100)) ? hit.point : Vector3.zero;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(this.name + " = " + other.name);
+        if (Vector3.Distance(this.transform.position, other.transform.position) < 0.5)
+            if (this.gameObject != null)
+                Destroy(this.gameObject);
+
     }
 }
